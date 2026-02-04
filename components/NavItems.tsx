a6369421e26f2854db90a731053b33867fd37ca6
robert thornton <robert.thornton92@gmail.com>
2026-02-04 15:23:54 +0200

    UI - change styling features

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -13,7 +13,7 @@ const NavItems:() => JSX.Element = () => {
     };
     
     return (
-        <ul className="nav-list">
+        <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
             {NAV_ITEMS.map(({href, label }) => (
                 <li key={href}>
                     <Link href={href} 
